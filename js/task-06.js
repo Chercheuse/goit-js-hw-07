072a66296e73c411d328e8b0120aab183762ee82
Chercheuse <108093435+Chercheuse@users.noreply.github.com>
2023-12-17 19:33:53 +0400

    task 6 done

--- js/task-06.js
+++ js/task-06.js
@@ -3,3 +3,30 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const input = document.querySelector("input");
+const boxes = document.getElementById("boxes");
+const createBtn = document.querySelector("button[data-create]");
+const destroyBtn = document.querySelector("button[data-destroy]");
+
+function createBoxes(amount) {
+  amount = input.value;
+
+  if (amount <= 100 && amount > 0) {
+    for (let i = 0; i < amount; i++) {
+      const newElement = document.createElement("div");
+      newElement.style.width = `${30 + i * 10}px`;
+      newElement.style.height = `${30 + i * 10}px`;
+      newElement.style.backgroundColor = getRandomHexColor();
+      boxes.appendChild(newElement);
+    }
+  }
+
+  input.value = "";
+}
+function destroyBoxes() {
+  boxes.innerHTML = "";
+}
+
+createBtn.addEventListener("click", createBoxes);
+destroyBtn.addEventListener("click", destroyBoxes);
