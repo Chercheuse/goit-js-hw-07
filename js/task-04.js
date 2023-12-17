9916a7306346ddfdbd632442ddd9f526c12c4fb4
Chercheuse <108093435+Chercheuse@users.noreply.github.com>
2023-12-17 18:32:53 +0400

    task 5 done

--- js/task-04.js
+++ js/task-04.js
@@ -1,1 +1,17 @@
+const form = document.querySelector(".login-form");
+const registerForm = form.addEventListener('submit', formValid)
 
+function formValid(event) {
+  event.preventDefault();
+
+  const form = event.target;
+  const login = form.elements.email;
+  const password = form.elements.password;
+
+  if (login.value === "" || password.value === "") {
+    alert("All form fields must be filled in");
+  }
+
+  console.log({email : login.value.trim(), password: password.value.trim()});
+  form.reset();
+}
