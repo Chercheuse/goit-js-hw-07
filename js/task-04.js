fffacbec6d94509909d3e38ef4af0c784e8bc27d
Chercheuse <108093435+Chercheuse@users.noreply.github.com>
2023-12-24 15:13:52 +0400

    improve the code for task 4

--- js/task-04.js
+++ js/task-04.js
@@ -5,13 +5,14 @@ function formValid(event) {
   event.preventDefault();
 
   const form = event.target;
-  const login = form.elements.email;
-  const password = form.elements.password;
+  const emailInput = form.elements.email.value.trim();
+  const passwordInput = form.elements.password.value.trim();
 
-  if (login.value === "" || password.value === "") {
-    alert("All form fields must be filled in");
+  if (emailInput === "" || passwordInput === "") {
+    alert("Усі поля форми повинні бути заповнені");
+    return;
   }
 
-  console.log({ email: login.value.trim(), password: password.value.trim() });
+  console.log({ email: emailInput, password: passwordInput });
   form.reset();
 }
